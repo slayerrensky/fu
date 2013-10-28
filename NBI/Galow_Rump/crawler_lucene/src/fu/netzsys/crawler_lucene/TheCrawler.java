a83e87a8c2f2cf7c5c6988d19b66826f823b943b
Dennis <flashbac0@gmail.com>
2013-10-28 20:41:51 +0100

    jsp page, search or crwal

--- NBI/Galow_Rump/crawler_lucene/src/fu/netzsys/crawler_lucene/TheCrawler.java
+++ NBI/Galow_Rump/crawler_lucene/src/fu/netzsys/crawler_lucene/TheCrawler.java
@@ -58,35 +58,37 @@ public class TheCrawler extends HttpServlet {
 		Crawler c = new Crawler();
 		String destSite = "leer";
 		Map parameters = request.getParameterMap();
-	    if (parameters.containsKey("search") && (request.getParameter("search") == "crawl") && parameters.containsKey("query")){
-	    	
-	    	destSite = request.getParameter("query");
-	    	System.out.println("Begin to crawl: " + destSite);
-	    	request.setAttribute("desti",request.getParameter("query"));
-	    	
-	    	//ArrayList<String> linkList = c.crawlAll(destSite, 2);
-	    	ArrayList<String> linkList = c.crawl(destSite, 0, 1);
-	    	Normalizer n = new Normalizer();
-	    	for (String link : linkList) {
-				URLInformation siteInfo = n.normalize(ContentByURL.getSiteContent(link));
-				siteInfo.setURL(link);
-				indexer.addToIndex(siteInfo);
-			}
-	    	System.out.println("crawler + indexer = fertig");
-	    	request.setAttribute("destination",linkList);
-	    }
-	    if(parameters.containsKey("search") && (request.getParameter("search") == "search") && parameters.containsKey("query"))
-	    {
-	    	LSearch search = new LSearch();
-	    	String query = request.getParameter("query");
-	    	LinkedList<URLInformation> founds = search.SearchForContent(query);
-	    	String ul = "<ul>\n";
-	    	for (URLInformation ulist : founds) {
-				ul += "<li><a href=\"" + ulist.getURL() + "\">" + ulist.getURL() + "</a>" + 
-					  "<p>" + ulist.getTitle() +"</p></li>"+"\n";
-			}
-	    	ul +="</ul>"+"\n";
-	    	request.setAttribute("linkliste",ul);
+	    if (parameters.containsKey("search") && parameters.containsKey("query")){
+	    	System.out.println("request");
+	    	String type = request.getParameter("search");
+	    	if(type.equals("crawl")){
+	    		destSite = request.getParameter("query");
+		    	System.out.println("Begin to crawl: " + destSite);
+		    	request.setAttribute("desti",request.getParameter("query"));
+		    	
+		    	//ArrayList<String> linkList = c.crawlAll(destSite, 2);
+		    	ArrayList<String> linkList = c.crawl(destSite, 0, 1);
+		    	Normalizer n = new Normalizer();
+		    	for (String link : linkList) {
+					URLInformation siteInfo = n.normalize(ContentByURL.getSiteContent(link));
+					siteInfo.setURL(link);
+					indexer.addToIndex(siteInfo);
+				}
+		    	System.out.println("crawler + indexer = fertig");
+		    	request.setAttribute("destination",linkList);	    		
+	    	}else{
+	    		LSearch search = new LSearch();
+	    		System.out.println("Begin to search");
+		    	String query = request.getParameter("query");
+		    	LinkedList<URLInformation> founds = search.SearchForContent(query);
+		    	String ul = "<ul>\n";
+		    	for (URLInformation ulist : founds) {
+					ul += "<li><a href=\"" + ulist.getURL() + "\">" + ulist.getURL() + "</a>" + 
+						  "<p>" + ulist.getTitle() +"</p></li>"+"\n";
+				}
+		    	ul +="</ul>"+"\n";
+		    	request.setAttribute("linkliste",ul);
+	    	}	
 	    }
 	    	
 	    RequestDispatcher view = request.getRequestDispatcher("/showInfo.jsp");
