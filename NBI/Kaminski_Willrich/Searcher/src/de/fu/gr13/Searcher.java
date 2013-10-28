2a7b9489c7edca53b5d03700779d3e9591696be0
PeteKaminski <p.kaminski00@gmail.com>
2013-10-28 20:14:24 +0100

    path mind F***

--- NBI/Kaminski_Willrich/Searcher/src/de/fu/gr13/Searcher.java
+++ NBI/Kaminski_Willrich/Searcher/src/de/fu/gr13/Searcher.java
@@ -41,7 +41,7 @@ public class Searcher extends HttpServlet {
 	Directory index; 
 	IndexWriterConfig config = null;
 	IndexWriter w = null;
-	String indexFile = "./webapps/[WBI-1314]-exercise21-group13/index";
+	String indexFile = "path/Library/Tomcat/webapps/[WBI-1314]-exercise21-group13/index";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
