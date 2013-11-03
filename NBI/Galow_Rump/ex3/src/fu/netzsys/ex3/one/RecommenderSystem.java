f54f0b51311d148ea05c25b173c2bd75e86cfdac
Dennis <flashbac0@gmail.com>
2013-11-03 17:09:34 +0100

    simuser to own class

--- NBI/Galow_Rump/ex3/src/fu/netzsys/ex3/one/RecommenderSystem.java
+++ NBI/Galow_Rump/ex3/src/fu/netzsys/ex3/one/RecommenderSystem.java
@@ -267,52 +267,6 @@ public class RecommenderSystem {
 	}
 }
 
-class SimilarUser implements Comparable<SimilarUser> {
-	Uuser user1 = null;
-	Uuser user2 = null;
-	double similarity = -2;
-
-	public SimilarUser(Uuser user1, Uuser user2, double similarity) {
-		super();
-		this.user1 = user1;
-		this.user2 = user2;
-		this.similarity = similarity;
-	}
-
-	public double getSimilarity() {
-		return similarity;
-	}
-
-	public void setSimilarity(double similarity) {
-		this.similarity = similarity;
-	}
-
-	public Uuser getUser1() {
-		return user1;
-	}
-
-	public void setUser1(Uuser user1) {
-		this.user1 = user1;
-	}
-
-	public Uuser getUser2() {
-		return user2;
-	}
-
-	public void setUser2(Uuser user2) {
-		this.user2 = user2;
-	}
-
-	@Override
-	public int compareTo(SimilarUser o) {
-		if (o.getSimilarity() > this.getSimilarity())
-			return 1;
-		else if (o.getSimilarity() == this.getSimilarity())
-			return 0;
-		return -1;
-	}
-}
-
 class MapUtil {
 	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(
 			Map<K, V> map) {
