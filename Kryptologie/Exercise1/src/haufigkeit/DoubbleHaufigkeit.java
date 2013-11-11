93df8ad41169c2a8c2c102c2070127292f1dabe5
Slayerrensky <rensky.g@googlemail.com>
2013-11-12 00:29:55 +0100

    add Haufigkeiten

---
+++ Kryptologie/Exercise1/src/haufigkeit/DoubbleHaufigkeit.java
@@ -0,0 +1,17 @@
+package haufigkeit;
+
+public class DoubbleHaufigkeit {
+
+	public String Letters;
+	/**
+	 * Wie oft die buchstabenkombi Promill mal vorkommt also %%
+	 */
+	public int Haufigkeit; 
+	
+	public DoubbleHaufigkeit(String letter, int haufigkeit)
+	{
+		Letters = letter;
+		Haufigkeit = haufigkeit;
+
+	}
+}
---
+++ Kryptologie/Exercise1/src/haufigkeit/English.java
@@ -0,0 +1,47 @@
+package haufigkeit;
+
+import java.util.ArrayList;
+
+static class English {
+	
+	public static ArrayList<DoubbleHaufigkeit> getDoubbleLetters(){
+		ArrayList<DoubbleHaufigkeit> dh = new ArrayList<DoubbleHaufigkeit>();
+		dh.add(new DoubbleHaufigkeit("LL",55));
+		dh.add(new DoubbleHaufigkeit("TT",53));
+		dh.add(new DoubbleHaufigkeit("SS",41));
+		dh.add(new DoubbleHaufigkeit("EE",39));
+		dh.add(new DoubbleHaufigkeit("PP",26));
+		dh.add(new DoubbleHaufigkeit("OO",23));
+		dh.add(new DoubbleHaufigkeit("RR",18));
+		dh.add(new DoubbleHaufigkeit("FF",14));
+		dh.add(new DoubbleHaufigkeit("CC",12));
+		dh.add(new DoubbleHaufigkeit("DD",10));	
+		return dh;
+	}
+	
+	public static ArrayList<DoubbleHaufigkeit> getBigrammLetters(){
+		ArrayList<DoubbleHaufigkeit> b = new ArrayList<DoubbleHaufigkeit>();
+		b.add(new DoubbleHaufigkeit("TH",315));
+		b.add(new DoubbleHaufigkeit("HE",251));
+		b.add(new DoubbleHaufigkeit("AN",172));
+		b.add(new DoubbleHaufigkeit("IN",169));
+		b.add(new DoubbleHaufigkeit("ER",154));
+		b.add(new DoubbleHaufigkeit("RE",148));
+		b.add(new DoubbleHaufigkeit("ON",145));
+		b.add(new DoubbleHaufigkeit("ES",145));
+		b.add(new DoubbleHaufigkeit("TI",128));
+		b.add(new DoubbleHaufigkeit("AT",124));
+		b.add(new DoubbleHaufigkeit("ST",121));
+		b.add(new DoubbleHaufigkeit("EN",120));
+		b.add(new DoubbleHaufigkeit("ND",118));
+		b.add(new DoubbleHaufigkeit("OR",113));
+		b.add(new DoubbleHaufigkeit("TO",111));
+		b.add(new DoubbleHaufigkeit("NT",110));
+		b.add(new DoubbleHaufigkeit("ED",107));
+		b.add(new DoubbleHaufigkeit("IS",106));
+		b.add(new DoubbleHaufigkeit("AR",101));
+		return b;
+	}
+}
+
+
