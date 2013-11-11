4543f0f0f5ea55474f80ae1c4f54557e49674c30
Slayerrensky <rensky.g@googlemail.com>
2013-11-11 22:06:56 +0100

    ready

--- Kryptologie/Exercise1/src/playfair/PlayFair.java
+++ Kryptologie/Exercise1/src/playfair/PlayFair.java
@@ -1,5 +1,10 @@
 package playfair;
 
+import java.awt.Dimension;
+import java.awt.geom.Dimension2D;
+
+import sun.org.mozilla.javascript.internal.ast.WithStatement;
+
 public class PlayFair {
 
 	static char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -51,10 +56,69 @@ public class PlayFair {
 	public static String encrypt(char[][] key, String clearText) {
 		String chiffre = "";
 		String[] split = clearText.split(" ");
-
+		
 		for (int i = 0; i < split.length; i++) {
-			// Verschluessel split[i]..
+
+			chiffre += getCryptLetters(key, split[i]) + " ";
 		}
+		
+		
 		return chiffre;
+	}
+	
+	
+	/**  y   mk
+	 *  0|x | k x x
+	 *  1|x x x x x
+	 *  2|x x x x x
+	 *  3|x m _ x x
+	 *  4|x x x x x
+	 *   ---------- x width
+	 *    0 1 2 3 4
+	 *   x = x  Jewels den Rechten Platz also x + 1 % 5
+	 *   y = y  Jewels den unteren platz also y + 1 % 5 
+	 *   x != y Kreuzung
+	 *   	m (1,3)    -> _ bei den höheren also (3,2) 
+	 *      k (2,0)    -> | bei den niedrigen also (1,0)  
+	 *   		
+	 *   2 buchstande erst y dann x  
+	 */
+	protected static String getCryptLetters(char[][] key, String s) {
+		char[] c = s.toCharArray();
+		
+		Dimension first = new Dimension(); 
+		Dimension secound = new Dimension(); 
+		
+		// find letter
+		for (int x = 0; x < key.length; x++) {
+			for (int y = 0; y < key[0].length; y++) {
+				if (c[0] == key[x][y]) {
+					first.height = y;
+					first.width = x;
+				}
+				if (c[1] == key[x][y]) {
+					secound.height = y;
+					secound.width = x;
+				}
+			}
+		}
+		
+		char[] out = new char[2];
+		
+		if(first.height == secound.height){  // y = y
+			out[0] =  key[first.height][(first.width + 1 ) % 5];
+			out[1] =  key[secound.height][(secound.width + 1 ) % 5];
+		} else if ( first.width == secound.width){ // x = x 
+			out[0] =  key[(first.height+ 1 ) % 5][first.width];
+			out[1] =  key[(secound.height+ 1 ) % 5][secound.width];
+		} else { //x != y Kreuzung
+			// Zeile beleibt gleich (y) height
+			// spalte des anderen 
+			
+			out[0] = key[first.width][secound.height]; //key[x][y] = key [width][height]
+			out[1] = key[secound.width][first.height];
+		}
+
+		return String.valueOf(out[0]) + String.valueOf(out[1]); 
 	}
 }
