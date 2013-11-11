6797b74310fd3ae453e3ab4d4bda2b2bc72cc985
Lars Willrich <larswillrich@gmail.com>
2013-11-11 19:31:26 +0100

    generatePlayFairQuadrat anfang

--- Kryptologie/Exercise1/src/playfair/Main.java
+++ Kryptologie/Exercise1/src/playfair/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		String[][] key = PlayFair.generatePlayFairQuadrat(KEY);
+		char[][] key = PlayFair.generatePlayFairQuadrat(KEY);
 		
 		String chiffreText = PlayFair.encrypt(key, CLEAR);
 		
--- Kryptologie/Exercise1/src/playfair/PlayFair.java
+++ Kryptologie/Exercise1/src/playfair/PlayFair.java
@@ -1,31 +1,45 @@
 package playfair;
 
-
 public class PlayFair {
 
 	public PlayFair() {
 	}
-	
-	
+
 	/**
-	 * @param key in From von "Schluessel" (Beispiel)
+	 * @param key
+	 *            in From von "Schluessel" (Beispiel)
 	 * @return die 5x5 Matrix
 	 */
-	public static String[][] generatePlayFairQuadrat(String key){
+	public static char[][] generatePlayFairQuadrat(String key) {
+		char[][] pfQuadrat = new char[5][5];
+
+		char[] charArray = key.toCharArray();
+		while (key != "") {
+			for (int x = 0; x < 5; x++) {
+				for (int y = 0; y < 5; y++) {
+					
+				}
+			}
+		}
+
 		return null;
 	}
 
 	/**
-	 * @param key key in Form einer 5x5 Matrix
-	 * @param clearText in Form von zwei Grossbuchstaben und Leerzeichen getrennt 	"AH CS YC ..."  (Beispiel)
-	 * @return Chiffre in Form von zwei Grossbuchstaben und Leerzeichen getrennt 	"VG VT SH ..."  (Beispiel)
+	 * @param key
+	 *            key in Form einer 5x5 Matrix
+	 * @param clearText
+	 *            in Form von zwei Grossbuchstaben und Leerzeichen getrennt
+	 *            "AH CS YC ..." (Beispiel)
+	 * @return Chiffre in Form von zwei Grossbuchstaben und Leerzeichen getrennt
+	 *         "VG VT SH ..." (Beispiel)
 	 */
-	public static String encrypt(String[][] key, String clearText) {
+	public static String encrypt(char[][] key, String clearText) {
 		String chiffre = "";
 		String[] split = clearText.split(" ");
-		
-		for (int i = 0;i<split.length;i++){
-			//Verschluessel split[i]..
+
+		for (int i = 0; i < split.length; i++) {
+			// Verschluessel split[i]..
 		}
 		return chiffre;
 	}
