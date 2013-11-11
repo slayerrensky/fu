242125b2134e5e57e3d579156d1f5c07ddc64995
Lars Willrich <larswillrich@gmail.com>
2013-11-11 19:44:31 +0100

    wie vorher

--- Kryptologie/Exercise1/src/playfair/PlayFair.java
+++ Kryptologie/Exercise1/src/playfair/PlayFair.java
@@ -2,6 +2,10 @@ package playfair;
 
 public class PlayFair {
 
+	static char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			'W', 'X', 'Y', 'Z' };
+	
 	public PlayFair() {
 	}
 
@@ -11,18 +15,27 @@ public class PlayFair {
 	 * @return die 5x5 Matrix
 	 */
 	public static char[][] generatePlayFairQuadrat(String key) {
+		if (key.length()>25) return null;
+		
 		char[][] pfQuadrat = new char[5][5];
 
-		char[] charArray = key.toCharArray();
+		char[] charArray = key.toUpperCase().toCharArray();
 		while (key != "") {
 			for (int x = 0; x < 5; x++) {
 				for (int y = 0; y < 5; y++) {
-					
+					if (key.length() > (x*5 + y)) pfQuadrat[x][y] = charArray[(x*5 + y)];
+					else{
+						pfQuadrat[x][y] = getNext(charArray[(x*5 + y)]);
+					}
 				}
 			}
 		}
 
 		return null;
+	}
+
+	private static char getNext(char c) {
+		return 0;
 	}
 
 	/**
