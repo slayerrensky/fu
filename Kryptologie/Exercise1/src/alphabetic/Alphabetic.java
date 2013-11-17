a90fca664f8f7bb6cbef1a453504d57749dd0554
= <=>
2013-11-17 14:12:37 +0100

    a

--- Kryptologie/Exercise1/src/alphabetic/Alphabetic.java
+++ Kryptologie/Exercise1/src/alphabetic/Alphabetic.java
@@ -1,7 +1,9 @@
 package alphabetic;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Alphabetic {
 	public static char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -10,10 +12,37 @@ public class Alphabetic {
 	public static final String ALPHABET_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	public static int getIndexOfLetter(char letter) {
-		return Arrays.asList(alphabet).indexOf(letter);
+		for (int i = 0; i < alphabet.length; i++) {
+			if (alphabet[i] == letter)
+				return i;
+		}
+		return -1;
 	}
 
 	public static char getLetterOfIndex(int index) {
 		return alphabet[index];
+	}
+
+	public static int[] frequenceAnalyzer(String str) {
+		str = str.toUpperCase();
+		int[] numbers = new int[alphabet.length];
+
+		char[] charArray = str.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			int index = getIndexOfLetter(charArray[i]);
+			numbers[index]++;
+		}
+		return numbers;
+	}
+
+	public static void printFrequentlyAnalytic(String str) {
+		int[] frequenceAnalyzer = Alphabetic.frequenceAnalyzer(str);
+		int sumOfLetters = str.length();
+
+		for (int i = 0; i < frequenceAnalyzer.length; i++) {
+			System.out.printf("%c : %3d : %6.2f %% \n", alphabet[i],
+					frequenceAnalyzer[i], (double) frequenceAnalyzer[i]
+							/ (double) sumOfLetters * 100);
+		}
 	}
 }
