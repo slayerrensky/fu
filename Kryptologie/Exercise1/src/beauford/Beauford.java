d5826c053c0413115fcc5cdec4e001b7062f27f0
Lars Willrich <larswillrich@gmail.com>
2013-11-18 13:37:34 +0100

    refactoring

--- Kryptologie/Exercise1/src/beauford/Beauford.java
+++ Kryptologie/Exercise1/src/beauford/Beauford.java
@@ -16,7 +16,8 @@ public class Beauford {
 	}
 
 	public static void main(String[] args) {
-		Beauford.kasiskiTest();
+//		Beauford.kasiskiTest();
+		a();
 	}
 
 	public static void kasiskiTest() {
@@ -41,10 +42,9 @@ public class Beauford {
 //		list.out();
 		list.printDistance();
 		
-		
-		// http://www.personal.psu.edu/users/m/r/mrk5094/Kasiski.html
-		
-		int kGV = 5;
+	}
+	
+	public static char[][] permutateForAnalytic(int kGV){
 		char[][] kasiskiList = new char[kGV][(int)(CIPHER.length()/kGV)+1]; 
  		
 		
@@ -54,12 +54,17 @@ public class Beauford {
 			kasiskiList[i%kGV][(int)(i/kGV)] = c;
 			i++;
 		}
-		
-		System.out.println(kasiskiList.length);
+		return kasiskiList;
+	}
+	
+	public static void a(){
+		// http://www.personal.psu.edu/users/m/r/mrk5094/Kasiski.html
+		int kGV = 5;
+		char[][] kasiskiList = permutateForAnalytic(kGV);
 		
 		// Listen zusammen basteln 2)
 		ArrayList<String> kasiski = new ArrayList<String>(); 
-		for(i=0; i < kGV; i++)
+		for(int i=0; i < kGV; i++)
 		{
 			String s = new String();
 			for (int j = 0; j < (int) (kasiskiList[0].length);j++)
@@ -79,7 +84,7 @@ public class Beauford {
 				
 			}
 			switch (i) {
-			case 1: // Zeile 2 könnte auch ein h als meist benutzen Buchstaben haben
+			case 1: // Zeile 2 koennte auch ein h als meist benutzen Buchstaben haben
 				s = s.replace((char)(l.get(0).getLetter() + 32), 'H');
 				s = s.replace((char)(l.get(1).getLetter() + 32), 'T');
 				//s = s.replace((char)(l.get(2).getLetter() + 32), 'A');
@@ -103,10 +108,10 @@ public class Beauford {
 			kasiski.add(s);
 		}
 		
-		// zurück formen
+		// zurueck formen
 		
 		String out = new String();
-		for (i = 0; i< (int)(CIPHER.length()/kGV+1); i++)
+		for (int i = 0; i< (int)(CIPHER.length()/kGV+1); i++)
 		{
 			for (int j=0;j<kGV;j++){
 				out += kasiski.get(j).substring(i,i+1);
@@ -116,13 +121,13 @@ public class Beauford {
 		System.out.println();
 		
 		int offset = 60;
-		for (i = 0; i< out.length(); i+=offset){
+		for (int i = 0; i< out.length(); i+=offset){
 			if (offset < out.length()-i)
 				System.out.println(out.substring(i,i+offset));
 			else 
 				System.out.println(out.substring(i,out.length()));
 		}
 		
-		// vielleicht könnte man mit den most common words etwas entwickeln was diese mit den text vergleicht.
+		// vielleicht koennte man mit den most common words etwas entwickeln was diese mit den text vergleicht.
 	}
 }
